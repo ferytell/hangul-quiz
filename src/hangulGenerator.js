2ad03c67b227c9211543166fd266dab57dddeb4a
ferytell <darkstat2709@gmail.com>
2026-04-10 00:03:47 +0700

    add syllables generator

---
+++ src/hangulGenerator.js
@@ -0,0 +1,108 @@
+const CHO = [
+  "ㄱ",
+  "ㄲ",
+  "ㄴ",
+  "ㄷ",
+  "ㄸ",
+  "ㄹ",
+  "ㅁ",
+  "ㅂ",
+  "ㅃ",
+  "ㅅ",
+  "ㅆ",
+  "ㅇ",
+  "ㅈ",
+  "ㅉ",
+  "ㅊ",
+  "ㅋ",
+  "ㅌ",
+  "ㅍ",
+  "ㅎ",
+];
+
+const JUNG = [
+  "ㅏ",
+  "ㅐ",
+  "ㅑ",
+  "ㅒ",
+  "ㅓ",
+  "ㅔ",
+  "ㅕ",
+  "ㅖ",
+  "ㅗ",
+  "ㅘ",
+  "ㅙ",
+  "ㅚ",
+  "ㅛ",
+  "ㅜ",
+  "ㅝ",
+  "ㅞ",
+  "ㅟ",
+  "ㅠ",
+  "ㅡ",
+  "ㅢ",
+  "ㅣ",
+];
+
+const JONG = [
+  "",
+  "ㄱ",
+  "ㄲ",
+  "ㄳ",
+  "ㄴ",
+  "ㄵ",
+  "ㄶ",
+  "ㄷ",
+  "ㄹ",
+  "ㄺ",
+  "ㄻ",
+  "ㄼ",
+  "ㄽ",
+  "ㄾ",
+  "ㄿ",
+  "ㅀ",
+  "ㅁ",
+  "ㅂ",
+  "ㅄ",
+  "ㅅ",
+  "ㅆ",
+  "ㅇ",
+  "ㅈ",
+  "ㅊ",
+  "ㅋ",
+  "ㅌ",
+  "ㅍ",
+  "ㅎ",
+];
+
+function generateSyllable(initial, vowel, final = "") {
+  const i = CHO.indexOf(initial);
+  const j = JUNG.indexOf(vowel);
+  const k = JONG.indexOf(final);
+
+  if (i === -1 || j === -1 || k === -1) return null;
+
+  const code = 0xac00 + i * 21 * 28 + j * 28 + k;
+  return String.fromCharCode(code);
+}
+
+export function generateSyllableData(consonants, vowels) {
+  const result = [];
+
+  consonants.forEach((c) => {
+    vowels.forEach((v) => {
+      const char = generateSyllable(c.char, v.char);
+
+      if (!char) return;
+
+      result.push({
+        char,
+        romaji: c.romaji.split("/")[0] + v.romaji, // simple combine
+        components: [c.char, v.char],
+        group: `cv-${c.char}`, // dynamic group
+      });
+    });
+  });
+
+  return result;
+}
